e4a4fd228820fb6afcf13326fd864d591860b700
刘振华 <liuzhenhua@syswin.com>
2017-11-18 14:38:14 +0800

    测试json

--- src/main/java/org/smallfire/java/json/TestMain.java
+++ src/main/java/org/smallfire/java/json/TestMain.java
@@ -1,23 +1,6 @@
 package org.smallfire.java.json;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-import com.bjtoon.framework.sdk.toon.tooncode.ToonDesUtils;
-import com.zhengtoon.bjtoon.conv.bean.ConfigDict;
-import com.zhengtoon.bjtoon.conv.bean.ConvAttachment;
-import com.zhengtoon.bjtoon.conv.bean.toonCode.VisitorInfo;
-import com.zhengtoon.bjtoon.conv.utils.AppConstant;
-import com.zhengtoon.bjtoon.conv.utils.CloudAppUtils;
-import com.zhengtoon.bjtoon.conv.utils.ZipUtils;
-import org.apache.commons.lang.time.DateUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
-import org.junit.Test;
-
-import java.io.File;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by Administrator on 2017/5/30.

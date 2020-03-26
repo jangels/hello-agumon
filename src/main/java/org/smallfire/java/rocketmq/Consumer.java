6cab64000140487dfe62c0a584c8b27f4b4a36d4
144637 <144637@syswin.com>
2020-03-26 11:58:26 +0800

    feture: 添加rockemq测试类

---
+++ src/main/java/org/smallfire/java/rocketmq/Consumer.java
@@ -0,0 +1,52 @@
+package org.smallfire.java.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer {
+
+    public static final String ADDR = "127.0.0.1:9876";
+
+    public static void main(String[] args) throws MQClientException {
+        //设置消费者组
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("CID_LRW_DEV_SUBS");
+
+        consumer.setVipChannelEnabled(false);
+        consumer.setNamesrvAddr(ADDR);
+        //设置消费者端消息拉取策略，表示从哪里开始消费
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        //设置消费者拉取消息的策略，*表示消费该topic下的所有消息，也可以指定tag进行消息过滤
+        consumer.subscribe("TopicTest", "*");
+
+        //消费者端启动消息监听，一旦生产者发送消息被监听到，就打印消息，和rabbitmq中的handlerDelivery类似
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                for (MessageExt messageExt : msgs) {
+                    String topic = messageExt.getTopic();
+                    String tag = messageExt.getTags();
+                    String msg = new String(messageExt.getBody());
+                    System.out.println("*********************************");
+                    System.out.println("消费响应：msgId : " + messageExt.getMsgId() + ",  msgBody : " + msg + ", tag:" + tag + ", topic:" + topic);
+                    System.out.println("*********************************");
+                }
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //调用start()方法启动consumer
+        consumer.start();
+        System.out.println("Consumer Started....");
+    }
+
+}
---
+++ src/main/java/org/smallfire/java/rocketmq/Producer.java
@@ -0,0 +1,43 @@
+package org.smallfire.java.rocketmq;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class Producer {
+
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+
+        //需要一个producer group名字作为构造方法的参数，这里为producer1
+        DefaultMQProducer producer = new DefaultMQProducer("producer1");
+
+        //设置NameServer地址,此处应改为实际NameServer地址，多个地址之间用；分隔
+        //NameServer的地址必须有，但是也可以通过环境变量的方式设置，不一定非得写死在代码里
+        producer.setNamesrvAddr(Consumer.ADDR);
+        producer.setVipChannelEnabled(false);
+
+        //为避免程序启动的时候报错，添加此代码，可以让rocketMq自动创建topickey
+        producer.setCreateTopicKey("AUTO_CREATE_TOPIC_KEY");
+        producer.start();
+
+        for(int i=0;i<10;i++){
+            try {
+                Message message = new Message("TopicTest", "Tag1",
+                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+                SendResult sendResult = producer.send(message);
+
+                System.out.println("发送的消息ID:" + sendResult.getMsgId() +"--- 发送消息的状态：" + sendResult.getSendStatus());
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
+
+        producer.shutdown();
+
+    }
+
+}

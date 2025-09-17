a3b7814f5544429a8842d741c6e9eda5705cc12b
刘振华2 <liuzhenhua3@songguo7.com>
2025-09-17 16:22:20 +0800

    feat: sensor

--- pygumon/spider/spider_01.py
+++ pygumon/spider/spider_01.py
@@ -1,13 +1,14 @@
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.marxists.org/chinese/maozedong/marxist.org-chinese-mao-19560714.htm"  # 以百度为例
+url = "https://www.baidu.com"  # 以百度为例
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
 response = requests.get(url, headers=headers)
-response.encoding = "gb2312"  # 百度使用 UTF-8 编码
+response.encoding = "UTF-8"  # 百度使用 UTF-8 编码
 
 soup = BeautifulSoup(response.text, "html.parser")
 title = soup.find("title").text
